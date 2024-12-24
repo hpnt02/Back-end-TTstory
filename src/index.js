6d3bec2c72e6f9c8f250df04265fbbe7033dcdb5
hpnt02 <hpnt02@gmail.com>
2024-12-25 02:53:57 +0700

    first

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.use(morgan('combined'))
 
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://tt-shop.vercel.app',
    headers: "content-type",
    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
      allowedHeaders: ['token','Content-Type', 'Authorization', 'access-control-allow-credentials','access-control-allow-methods','access-control-allow-origin','access-control-allow-headers'],
